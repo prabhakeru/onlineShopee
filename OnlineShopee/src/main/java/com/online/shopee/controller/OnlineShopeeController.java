40d7b47ff13b77bce82e361826379db8e9d81fb1
prabhakeru <prabhaker.ujinpaad@gmail.com>
2020-03-01 18:37:17 +0530

    Update OnlineShopeeController.java

--- OnlineShopee/src/main/java/com/online/shopee/controller/OnlineShopeeController.java
+++ OnlineShopee/src/main/java/com/online/shopee/controller/OnlineShopeeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -15,9 +17,12 @@ public class OnlineShopeeController {
     @Autowired
     private ProductService productService;
 
-    @RequestMapping(method = RequestMethod.GET,path = "/onlineshopee/getSKU")
+    @RequestMapping(method = RequestMethod.GET,path = "/retrieveAllProducts")
+    @ResponseBody
     public List<Product> getAllProducts(){
-        List<Product> products = productService.getAllProducts();
+        //List<Product> products = productService.getAllProducts();
+        List<Product> products  = new ArrayList<>();
+        products.add(new Product(123,1 ,"SHIRT","RED","m"));
         return products;
     }
 
